e6c9b03ee7c31e19c3ad3db58c8febb35288717c
hly920928 <hly920928@gmail.com>
2018-08-12 19:04:41 +0800

    LeetCode 225. Implement Stack using Queues

--- LeetCode.h
+++ LeetCode.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <stack>
+#include <queue>
 #include <vector>
 #include <map>
 #include <unordered_map>
@@ -494,3 +495,69 @@ int countNodes(TreeNode* root);
 int computeArea(int A, int B, int C, int D, int E, int F, int G, int H);
 //224. Basic Calculator
 int calculate_my(std::string s);
+class MyStack {
+public:
+	/** Initialize your data structure here. */
+	MyStack() {
+
+	}
+
+	/** Push element x onto stack. */
+	void push(int x) {
+
+	}
+
+	/** Removes the element on top of the stack and returns that element. */
+	int pop() {
+
+	}
+
+	/** Get the top element. */
+	int top() {
+
+	}
+
+	/** Returns whether the stack is empty. */
+	bool empty() {
+
+	}
+}; 
+//225. Implement Stack using Queues
+class MyStack_q {
+private:
+	std::queue<int>q;
+public:
+	/** Initialize your data structure here. */
+	MyStack_q() {};
+
+	/** Push element x onto stack. */
+	void push(int x) {
+		q.push(x);
+	}
+
+	/** Removes the element on top of the stack and returns that element. */
+	int pop() {
+		int n = q.size();
+		for (int i = 0; i < n - 1; i++) {
+			q.push(q.front()); q.pop();
+		}
+		int ans = q.front(); q.pop();
+		return ans;
+	}
+
+	/** Get the top element. */
+	int top() {
+		int n = q.size();
+		for (int i = 0; i < n - 1; i++) {
+			q.push(q.front()); q.pop();
+		}
+		int ans = q.front(); q.pop();
+		q.push(ans);
+		return ans;
+	}
+
+	/** Returns whether the stack is empty. */
+	bool empty() {
+		return q.empty();
+	}
+};
