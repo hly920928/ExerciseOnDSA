b99328e5e2a2e43b396546ed5dc752205080ef58
hly920928 <hly920928@gmail.com>
2018-10-12 23:37:52 +0800

    LeetCode 327. Count of Range Sum
    New Project Begin

---
+++ LeetCode/LeetCode/LeetCode.cpp
@@ -0,0 +1,14 @@
+// LeetCode.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "myHeader.h"
+#include <vector>
+using namespace std;
+int main()
+{ 
+	std::vector<int>nums({ 1,2,3 });
+	countRangeSum(nums, 1, 2);
+    return 0;
+}
+
---
+++ LeetCode/LeetCode/mySource.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+#include "myHeader.h"
+#include "iostream"
+
+using namespace std;
+int countRangeSum(std::vector<int>& nums, int lower, int upper) {
+	cout << "OK";
+	return 0;
+}
