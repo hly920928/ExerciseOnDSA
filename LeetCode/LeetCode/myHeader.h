488cd866aaf252203ad55635855971001e8962e2
hly920928 <hly920928@gmail.com>
2018-11-21 23:57:59 +0800

    LeetCode 355. Design Twitter
    API

--- LeetCode/LeetCode/myHeader.h
+++ LeetCode/LeetCode/myHeader.h
@@ -150,3 +150,22 @@ public:
 		return ans;
 	}
 };
+//355. Design Twitter
+class Twitter {
+public:
+	Twitter() {
+
+	}
+	void postTweet(int userId, int tweetId) {
+
+	}
+	vector<int> getNewsFeed(int userId) {
+
+	}
+	void follow(int followerId, int followeeId) {
+
+	}
+	void unfollow(int followerId, int followeeId) {
+
+	}
+};
