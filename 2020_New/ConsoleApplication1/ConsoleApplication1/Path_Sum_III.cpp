7a5ccc6a8c929070faa76383b6f46d6693fb81cc
hly <hly920928@gmail.com>
2020-01-09 20:10:31 +0800

    LeetCode 437. Path Sum III,store previous path sum to reduce resursive branching,a little faster

--- 2020_New/ConsoleApplication1/ConsoleApplication1/Path_Sum_III.cpp
+++ 2020_New/ConsoleApplication1/ConsoleApplication1/Path_Sum_III.cpp
@@ -1,6 +1,6 @@
 #include "mainHeader.h"
 #include <map>
- 
+using namespace std;
 struct TreeNode {
     int val;
     TreeNode* left;
@@ -9,6 +9,19 @@ struct TreeNode {
 };
 
  
+void pathSum_re_II(TreeNode* root, int sum,int pre_sum,int&  ans, map<int, int>&table) {
+    if (root == nullptr)return;
+    int now_sum = pre_sum + root->val;
+    auto itr = table.find(now_sum - sum);
+    if (itr != table.end())ans++;
+      itr = table.find(now_sum);
+    if (itr == table.end())table[now_sum] = 1;
+    else table[now_sum]++;
+    pathSum_re_II(root->left, sum, now_sum, ans, table);
+    pathSum_re_II(root->right, sum, now_sum,ans, table);
+    if (table[now_sum] == 1)table.erase(now_sum);
+    else table[now_sum]--;
+}
 
 int pathSum_re(TreeNode* root, int sum, bool isInvolved) {
     if (root == nullptr)return 0;
@@ -28,5 +41,10 @@ int pathSum_re(TreeNode* root, int sum, bool isInvolved) {
 
 }
 int pathSum(TreeNode* root, int sum) {
-    return pathSum_re(root, sum, false);
+    int  ans = 0;
+    map<int, int> table;
+    //return pathSum_re(root, sum, false);
+      table[0] = 1;
+      pathSum_re_II(root, sum, 0, ans, table);
+      return ans;
 }
