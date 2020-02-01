04ad11cf6775093410b9c91aa9290c9cebc868d6
hly <hly920928@gmail.com>
2020-02-01 23:56:13 +0800

    Update Delete_Node_in_a_BST.cpp
    Finish search

--- 2020_New/ConsoleApplication1/ConsoleApplication1/Delete_Node_in_a_BST.cpp
+++ 2020_New/ConsoleApplication1/ConsoleApplication1/Delete_Node_in_a_BST.cpp
@@ -1,15 +1,51 @@
 //450. Delete Node in a BST
 #include "mainHeader.h"
-void searchNode(TreeNode* root, int key,TreeNode*& keyNode, TreeNode*& parentNode, bool& isRightChild) {
+void searchNode(TreeNode* root, int key,TreeNode*& keyNode, TreeNode** parentNodeLink,bool& isMax,bool& isMin) {
+	if (root == nullptr)return;
+	if (root->val == key) {
+		keyNode = root;//special case root
+		return;
+	}
+	if (root->left->val == key) {
+		keyNode = root->left ;
+		parentNodeLink=&root->left;
+		return;
+	}
+	if (root->right->val == key) {
+		keyNode = root->right;
+		parentNodeLink = &root->left;
+		return;
+	}
+	if (root->val < key) {
+		isMin = false;
+		searchNode(root->left, key, keyNode, parentNodeLink, isMax, isMin);
+		return;
+	}
+	if (root->val >key) {
+		isMax= false;
+		searchNode(root->right, key, keyNode, parentNodeLink, isMax, isMin);
+		return;
+	}
+	return;
+}
+void findNext(TreeNode* keyNode, TreeNode*& nextNode) {
+
 
 }
-void reconstructAt(TreeNode* keyNode, TreeNode* parentNode,bool isRightChild) {
+void reconstructAt(TreeNode* keyNode, TreeNode** parentNodeLink, TreeNode*  nextNode) {
 
 }
+
 TreeNode* deleteNode(TreeNode* root, int key) {
-	TreeNode* keyNode;
-	TreeNode* parentNode;
-	bool isRightChild;
-	searchNode(root, key, keyNode, parentNode, isRightChild);
-	reconstructAt(keyNode, parentNode, isRightChild);
+	TreeNode* keyNode = nullptr;
+	TreeNode**  parentNodeLink=nullptr;
+
+	bool isMax = true; bool isMin = true;
+
+	TreeNode* nextNode;
+ 
+	searchNode(root, key, keyNode, parentNodeLink);
+	if (keyNode == nullptr)return nullptr;
+	findNext(keyNode, nextNode);
+	reconstructAt(keyNode, parentNodeLink, nextNode);
 }
