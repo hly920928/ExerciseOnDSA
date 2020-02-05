95fce4d41650e4865aecec3a6e3cf1bbd66cd075
hly <hly920928@gmail.com>
2020-02-05 23:57:21 +0800

    Update Delete_Node_in_a_BST.cpp
    Finished But Bug 80/85

--- 2020_New/ConsoleApplication1/ConsoleApplication1/Delete_Node_in_a_BST.cpp
+++ 2020_New/ConsoleApplication1/ConsoleApplication1/Delete_Node_in_a_BST.cpp
@@ -1,32 +1,42 @@
 //450. Delete Node in a BST
 #include "mainHeader.h"
-void searchNode(TreeNode* root, int key,TreeNode*& keyNode, TreeNode** parentNodeLink,bool& isMax,bool& isMin) {
-	if (root == nullptr)return;
+void searchNode(TreeNode* root, int key,TreeNode*& keyNode, TreeNode**& parentNodeLink,bool& isMax,bool& isMin) {
+	if (root == nullptr) {
+		keyNode = nullptr;
+		return;
+	}
 	if (root->val == key) {
 		keyNode = root;//special case root
 		return;
 	}
-	if (root->left->val == key) {
+	if (root->right != nullptr && root->right == nullptr) {
+		keyNode = nullptr;
+		return;
+	}
+	if (root->left !=nullptr&&root->left->val == key) {
+		isMax = false;
 		keyNode = root->left ;
 		parentNodeLink=&root->left;
 		return;
 	}
-	if (root->right->val == key) {
+	if (root->right != nullptr && root->right->val == key) {
+		isMin = false;
 		keyNode = root->right;
-		parentNodeLink = &root->left;
+		parentNodeLink = &root->right;
 		return;
 	}
 	if (root->val < key) {
 		isMin = false;
-		searchNode(root->left, key, keyNode, parentNodeLink, isMax, isMin);
+		searchNode(root->right, key, keyNode, parentNodeLink, isMax, isMin);
 		return;
 	}
 	if (root->val >key) {
 		isMax= false;
-		searchNode(root->right, key, keyNode, parentNodeLink, isMax, isMin);
+		searchNode(root->left, key, keyNode, parentNodeLink, isMax, isMin);
 		return;
 	}
-	return;
+	keyNode = nullptr;
+	return ;
 }
 void findNextAndRemove(TreeNode* keyNode, TreeNode*& nextNode) {
 	if (keyNode->right == nullptr) {
@@ -38,6 +48,7 @@ void findNextAndRemove(TreeNode* keyNode, TreeNode*& nextNode) {
 	if (now->left == nullptr) {
 		nextNode = now;
 		parent->right = now->right;
+		return;
 	}
 	while(now->left != nullptr) {
 		parent = now;
@@ -47,20 +58,38 @@ void findNextAndRemove(TreeNode* keyNode, TreeNode*& nextNode) {
 	nextNode= now;
 	parent->left = now->right;
  }
-void reconstructAt(TreeNode* keyNode, TreeNode** parentNodeLink, TreeNode*  nextNode) {
-
-}
+ 
 
 TreeNode* deleteNode(TreeNode* root, int key) {
+	if (root == nullptr)return nullptr;
 	TreeNode* keyNode = nullptr;
-	TreeNode**  parentNodeLink=nullptr;
+	TreeNode** parentNodeLink = nullptr;
 
 	bool isMax = true; bool isMin = true;
 
 	TreeNode* nextNode;
+	if (root->val == key) {
+		if (root->left == nullptr && root->right == nullptr)return nullptr;
+		if (root->left == nullptr)return root->right;
+		if (root->right == nullptr)return root->left;
+		findNextAndRemove(root, nextNode);
+		root->val = nextNode->val;
+		return root;
+	}
+
  
-	searchNode(root, key, keyNode, parentNodeLink);
-	if (keyNode == nullptr)return nullptr;
-	findNext(keyNode, nextNode);
-	reconstructAt(keyNode, parentNodeLink, nextNode);
+	searchNode(root, key, keyNode, parentNodeLink, isMax, isMin);
+	if (keyNode == nullptr)return root;
+	if (isMax) {
+		*parentNodeLink = keyNode->left;
+		return root;
+	}
+	if (keyNode->right == nullptr) {
+		*parentNodeLink = keyNode->left;
+		return root;
+	}
+	findNextAndRemove(keyNode, nextNode);
+	if (nextNode != nullptr)keyNode->val = nextNode->val;
+	else *parentNodeLink = nullptr;
+	return root;
 }
