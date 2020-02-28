d3d3baf3e5a2c7d079a5e2c460e938d1ada5e706
hly <hly920928@gmail.com>
2020-02-28 17:50:25 +0800

    LeetCode 472. Concatenated Words
    Build Tries Then DPS

--- 2020_New/ConsoleApplication1/ConsoleApplication1/Concatenated_Words.cpp
+++ 2020_New/ConsoleApplication1/ConsoleApplication1/Concatenated_Words.cpp
@@ -5,15 +5,54 @@ vector<string>* _w;
 class TriesNode {
 public:
 	TriesNode* next[26];
-	TriesNode() {
+	bool hasWords;
+	TriesNode() :hasWords(false) {
 		for (auto& p : next)p = nullptr;
 	}
 };
 void addToTries(const string& str, TriesNode* root) {
+	auto now = root;
+	for (int i = 0; i < str.size(); i++) {
+		if (now->next[str[i] - 'a'] == nullptr) {
+			now->next[str[i] - 'a'] = new TriesNode();
 
+		}
+		if (i == str.size() - 1) {
+			now->next[str[i] - 'a']->hasWords = true;
+		}
+		now = now->next[str[i] - 'a'];
+	}
 }
 bool isConcat(const string& str, TriesNode* root) {
-	return false;
+	auto now = root; 
+	int i = 0;
+	while (i < str.size()) {
+		if (now->next[str[i] - 'a'] == nullptr) {
+			if (!now->hasWords)return false;
+			else 	now = root;
+		}
+		else {
+			now=now->next[str[i] - 'a'];
+			i++;
+		}
+	}
+	return true;
+}
+
+bool isConcat_DFS(const string& str,  TriesNode* root,TriesNode* now,int i) {
+ 
+	if (i >= str.size())return false;
+	if (i == str.size() - 1) {
+		char last = str[i];
+		if (now->next[last - 'a'] == nullptr)return false;
+		else return now->next[last - 'a']->hasWords;
+	}
+	auto next = now->next[str[i] - 'a'];
+	if (next == nullptr)return false;
+	if (next->hasWords) {
+		if (isConcat_DFS(str, root, root, i + 1))return true;
+	}
+	return isConcat_DFS(str, root, next, i + 1);
 }
 bool isLonger(  int a,int b) {
 	const auto& words = *_w;
@@ -21,16 +60,16 @@ bool isLonger(  int a,int b) {
 }
 vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
 	vector<string> ans;
+	if (words.size() == 0)return ans;
 	_w = &words;
 	vector<int>index; index.resize(words.size());
-	for (int i = 0; i < words.size(); i++) {
-		index[i] = i;
-	}
+	for (int i = 0; i < words.size(); i++) index[i] = i;
 	sort(index.begin(), index.end(), isLonger);
 	TriesNode root;
 	for (int i = 0; i < words.size(); i++) {
 		string& now = words[index[i]];
-		if (isConcat(now, &root))ans.push_back(now);
+		if (now.size() == 0)continue;
+		if (isConcat_DFS(now, &root, &root,0))ans.push_back(now);
 		addToTries(now, &root);
 	}
 	return ans;
