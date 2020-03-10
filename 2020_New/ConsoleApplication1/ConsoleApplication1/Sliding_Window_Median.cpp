3d1253cf0719ee9934ad2ed107c83b2f370fc1f7
hly <hly920928@gmail.com>
2020-03-10 19:05:41 +0800

    Update Sliding_Window_Median.cpp
    Finish case when k is odd ,simply tested

--- 2020_New/ConsoleApplication1/ConsoleApplication1/Sliding_Window_Median.cpp
+++ 2020_New/ConsoleApplication1/ConsoleApplication1/Sliding_Window_Median.cpp
@@ -5,35 +5,52 @@
 using namespace std;
 void medianSlidingWindowOdd(vector<int>& nums, int k, vector<double>& ans) {
 	multiset<int> table;
-	for (int i = 0; i < k; i++) {
-		table.insert(nums[k]);
-	}
+	for (int i = 0; i < k; i++) table.insert(nums[i]);
 	auto median = table.begin();
-	for (int i = 0; i < (k - 1 / 2); i++)median++;
+	for (int i = 0; i < (k - 1) / 2; i++)median++;
 	ans.push_back(*median);
-	for (int i = 1; i + k < nums.size(); i++) {
+	for (int i = 1; i + k <= nums.size(); i++) {
 		int  deletedElement = nums[i - 1];
 		int  insertedElement = nums[i + k - 1];
-		table.erase(deletedElement);
-		table.insert(insertedElement);
+		int m = *median;
 		//TODO change median according to relation between median,deletedElement,insertedElement
-
-		//
+		if (deletedElement ==m) {
+			if (insertedElement == m)goto OUT;
+			if (insertedElement != m) {	
+				table.insert(insertedElement);
+				if ( insertedElement>m) median++;
+				else if (insertedElement<m) median--;
+				table.erase(deletedElement);
+			}
+		}
+		else if(deletedElement > m) {
+			table.insert(insertedElement);
+			table.erase(deletedElement);
+			if (insertedElement >= m)goto OUT;
+		    if (insertedElement < m) median--;
+		}
+		else if (deletedElement <m ) {
+			table.insert(insertedElement);
+			table.erase(deletedElement);
+			if (insertedElement< m) goto OUT;
+			if (insertedElement >= m) median++;
+			}
+ 
+	 OUT:
+ 
 		ans.push_back(*median);
 	}
 }
 void medianSlidingWindowEven(vector<int>& nums, int k, vector<double>& ans) {
 	multiset<int> table;
-	for (int i = 0; i < k; i++) {
-		table.insert(nums[k]);
-	}
+	for (int i = 0; i < k; i++) table.insert(nums[i]);
 	auto median_1 = table.begin();
 	auto median_2 = median_1; median_2++;
-	for (int i = 0; i < (k - 1 / 2); i++) {
+	for (int i = 0; i < (k - 1) / 2; i++) {
 		median_1++; median_2++;
 	}
 	ans.push_back((*median_1+ *median_2)/2);
-	for (int i = 1; i + k < nums.size(); i++) {
+	for (int i = 1; i + k <= nums.size(); i++) {
 		int  deletedElement = nums[i - 1];
 		int  insertedElement = nums[i + k - 1];
 		table.erase(deletedElement);
