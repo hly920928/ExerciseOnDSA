5aec4bfe06517b6bb3dd61f1ea93b2ee4cea879a
hly <hly920928@gmail.com>
2020-03-15 19:51:07 +0800

    Update Sliding_Window_Median.cpp
    Finshing Debugging,
    Runtime: 64 ms, faster than 85.73%

--- 2020_New/ConsoleApplication1/ConsoleApplication1/Sliding_Window_Median.cpp
+++ 2020_New/ConsoleApplication1/ConsoleApplication1/Sliding_Window_Median.cpp
@@ -3,6 +3,11 @@
 #include <set>
  
 using namespace std;
+void eraseLast(multiset<int>& table,int val) {
+	auto itr = table.upper_bound(val);
+	itr--;
+	table.erase(itr);
+}
 void medianSlidingWindowOdd(vector<int>& nums, int k, vector<double>& ans) {
 	multiset<int> table;
 	for (int i = 0; i < k; i++) table.insert(nums[i]);
@@ -18,20 +23,23 @@ void medianSlidingWindowOdd(vector<int>& nums, int k, vector<double>& ans) {
 			if (insertedElement == m)goto OUT;
 			if (insertedElement != m) {	
 				table.insert(insertedElement);
+				median--;
+				eraseLast(table, deletedElement);
 				if ( insertedElement>m) median++;
-				else if (insertedElement<m) median--;
-				table.erase(deletedElement);
+				else if (insertedElement<m) goto OUT;
+			
+		 
 			}
 		}
 		else if(deletedElement > m) {
 			table.insert(insertedElement);
-			table.erase(deletedElement);
+			eraseLast(table, deletedElement);
 			if (insertedElement >= m)goto OUT;
 		    if (insertedElement < m) median--;
 		}
 		else if (deletedElement <m ) {
 			table.insert(insertedElement);
-			table.erase(deletedElement);
+			eraseLast(table, deletedElement);
 			if (insertedElement< m) goto OUT;
 			if (insertedElement >= m) median++;
 			}
@@ -49,12 +57,16 @@ void medianSlidingWindowEven(vector<int>& nums, int k, vector<double>& ans) {
 	for (int i = 0; i < (k - 1) / 2; i++) {
 		median_1++; median_2++;
 	}
-	ans.push_back((*median_1+ *median_2)/2);
+	ans.push_back(((double)*median_1 + (double)*median_2) / 2);
 	for (int i = 1; i + k <= nums.size(); i++) {
 		int  deletedElement = nums[i - 1];
 		int  insertedElement = nums[i + k - 1];
 		int m1 = *median_1;
 		int m2 = *median_2;
+		if (k == 2) {
+			ans.push_back(((double)nums[i] + (double)nums[i+1]) / 2);
+			continue;
+		}
 		//TODO change median according to relation between median_1,median_2,deletedElement,insertedElement
 		if (deletedElement==m1|| deletedElement == m2) {
 			if (deletedElement == m1 && deletedElement == m2) {
@@ -62,13 +74,13 @@ void medianSlidingWindowEven(vector<int>& nums, int k, vector<double>& ans) {
 					table.insert(insertedElement);
 					median_1--;
 					median_2--;
-					table.erase(deletedElement);
+					eraseLast(table, deletedElement);
 				}
 				else if (insertedElement >=m1) {
 					median_1--;
 					median_2--;
 					table.insert(insertedElement);
-					table.erase(deletedElement);
+					eraseLast(table, deletedElement);
 					median_1++;
 					median_2++;
 				}
@@ -76,14 +88,15 @@ void medianSlidingWindowEven(vector<int>& nums, int k, vector<double>& ans) {
 			else if (deletedElement == m1) {
 				if (insertedElement < m2) {
 					table.insert(insertedElement);
-					median_1--;
-					table.erase(deletedElement);
+					eraseLast(table, deletedElement);
+					median_1 = median_2; median_1--;
+	
 				}
 				else if (insertedElement >= m2) {
 					table.insert(insertedElement);	
 					median_1++;
 					median_2++;
-					table.erase(deletedElement);
+					eraseLast(table, deletedElement);
 				}
 			}
 			else if (deletedElement == m2) {
@@ -91,28 +104,29 @@ void medianSlidingWindowEven(vector<int>& nums, int k, vector<double>& ans) {
 					table.insert(insertedElement);
 					median_1--;
 					median_2--;
-					table.erase(deletedElement);
+					eraseLast(table, deletedElement);
 				}
 				else if (insertedElement >= m1) {
 					table.insert(insertedElement);
-					median_2++;
-					table.erase(deletedElement);
+					eraseLast(table, deletedElement);
+					median_2 = median_1; median_2++;
+				
 				}
 			}
 		}
 		else if (deletedElement > m2) {
 			if(insertedElement>=m2){
-				table.erase(deletedElement);
+				eraseLast(table, deletedElement);
 				table.insert(insertedElement);
 				 
 			}
 			else if(insertedElement< m2&& insertedElement >=m1) {
-				table.erase(deletedElement);
+				eraseLast(table, deletedElement);
 				table.insert(insertedElement);
 				median_2--;
 			}
 			else if( insertedElement < m1) {
-				table.erase(deletedElement);
+				eraseLast(table, deletedElement);
 				table.insert(insertedElement);
 				median_1--;
 				median_2--;
@@ -120,25 +134,25 @@ void medianSlidingWindowEven(vector<int>& nums, int k, vector<double>& ans) {
 		}
 		else if (deletedElement <m1) {
 			if (insertedElement >= m2) {
-				table.erase(deletedElement);
+				eraseLast(table, deletedElement);
 				table.insert(insertedElement);
 				median_1++;
 				median_2++;
 			}
 			else if (insertedElement < m2 && insertedElement >= m1) {
-				table.erase(deletedElement);
+				eraseLast(table, deletedElement);
 				table.insert(insertedElement);
 				median_1++;
 			}
 			else if (insertedElement < m1) {
-				table.erase(deletedElement);
+				eraseLast(table, deletedElement);
 				table.insert(insertedElement);
 			}
 		}
 		//
 		OUT:
 		ans.push_back(((double)*median_1 + (double)*median_2) / 2);
-		//table.erase(deletedElement);
+		//eraseLast(table, deletedElement);
 		//table.insert(insertedElement);
 	}
 }
