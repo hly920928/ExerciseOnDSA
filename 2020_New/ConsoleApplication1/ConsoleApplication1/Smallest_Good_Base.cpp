3e9550bb09b21742a75592128f621771952378c2
hly <hly920928@gmail.com>
2020-03-20 19:22:01 +0800

    Update Smallest_Good_Base.cpp
    changing method,linear search digit length,binary search base

--- 2020_New/ConsoleApplication1/ConsoleApplication1/Smallest_Good_Base.cpp
+++ 2020_New/ConsoleApplication1/ConsoleApplication1/Smallest_Good_Base.cpp
@@ -1,13 +1,20 @@
 //483. Smallest Good Base
 #include "mainHeader.h"
 using namespace std;
-bool isGoodBase(unsigned long long n, int base) {
-	return false;
+int binarySearch_re(unsigned long long n, int digit,int lo,int hi) {
+	return -1;
 }
-int binarySearch_re(unsigned long long n, unsigned long long lo, unsigned long long hi) {
-	return 0;
-}
+int findSmallestGoodBase(unsigned long long n,int digit) {
+	int ans = binarySearch_re(n, digit,2,n);
+	if (ans != -1)return ans;
+	return -1;
+ }
 string smallestGoodBase(string n) {
 	unsigned long long _n = strtoull(n.data(),NULL,10);
-	return to_string(binarySearch_re(_n, 2, _n));
+	int max_digit = log2(_n);
+	for (int i = max_digit; i >= 1; i--) {
+		int ans = findSmallestGoodBase(_n, i);
+		if (ans != -1)return to_string(ans);
+	}
+	return to_string(-1);
 }
