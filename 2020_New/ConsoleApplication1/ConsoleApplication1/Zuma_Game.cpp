d4a80977bea8b8837f480168b67b0839b3bd02e0
hly <hly920928@gmail.com>
2020-03-27 22:50:04 +0800

    Update Zuma_Game.cpp
    Finish state constructor

--- 2020_New/ConsoleApplication1/ConsoleApplication1/Zuma_Game.cpp
+++ 2020_New/ConsoleApplication1/ConsoleApplication1/Zuma_Game.cpp
@@ -11,7 +11,14 @@ public:
 	char counts;
 	balls(char cr = 5, char ct = 0) : color(cr), counts(ct) {};
 };
-
+inline char colorToId(char c) {
+	if (c == 'R')return 0;
+	if (c == 'Y')return 1;
+	if (c == 'B')return 2;
+	if (c == 'G')return 3;
+	if (c == 'W')return 4;
+	return -1;
+}
 class state {
 private:
 	vector<balls> table;
@@ -19,8 +26,21 @@ private:
 	int depth;
 	int remain;
 public:
-	state(string& board, string& hand) {};
-	bool operator<(state& b)const  {
+	state(string& board, string& hand) {
+		table.push_back(balls(colorToId(board[0]),1));
+		for (int i = 1; i < board.size(); i++) {
+			if (board[i] == board[i - 1]) {
+				table[table.size() - 1].counts++;
+			}
+			else {
+				table.push_back(balls(colorToId(board[i]), 1));
+			}
+		}
+		for (int& i : hands) i=0;
+		for (char c : hand)hands[colorToId(c)]++;
+	
+	};
+	bool operator<(const state& b)const  {
 		if (depth > b.depth)return true;
 		if (depth < b.depth)return false;
 		if (remain > b.remain)return true;
