4b5051c00b2bb71ad960fdb17a298244ada9634a
hly <hly920928@gmail.com>
2020-03-28 17:45:15 +0800

    Update Zuma_Game.cpp
    Finish produceNextAndTestEmpty()

--- 2020_New/ConsoleApplication1/ConsoleApplication1/Zuma_Game.cpp
+++ 2020_New/ConsoleApplication1/ConsoleApplication1/Zuma_Game.cpp
@@ -11,6 +11,8 @@ public:
 	char counts;
 	balls(char cr = 5, char ct = 0) : color(cr), counts(ct) {};
 };
+
+
 inline char colorToId(char c) {
 	if (c == 'R')return 0;
 	if (c == 'Y')return 1;
@@ -26,7 +28,12 @@ private:
 	int depth;
 	int remain;
 public:
+	state() :depth(0), remain(0) {
+		for (int& i : hands) i = 0;
+	}
 	state(string& board, string& hand) {
+		depth = 0; remain = board.size();
+		for (int& i : hands) i = 0;
 		table.push_back(balls(colorToId(board[0]),1));
 		for (int i = 1; i < board.size(); i++) {
 			if (board[i] == board[i - 1]) {
@@ -36,7 +43,7 @@ public:
 				table.push_back(balls(colorToId(board[i]), 1));
 			}
 		}
-		for (int& i : hands) i=0;
+		
 		for (char c : hand)hands[colorToId(c)]++;
 	
 	};
@@ -48,15 +55,34 @@ public:
 		return false;
 	}
 	bool produceNextAndTestEmpty(priority_queue<state>& pq) {
+		vector<int>pos_1; vector<int>pos_2;
+		for (int i = 0; i < table.size(); i++) {
+			char cr = table[i].color;
+			char ct = table[i].counts;
+			if (ct == 1 && hands[cr] >= 2)pos_1.push_back(i);
+			if (ct ==2 && hands[cr] >= 1)pos_2.push_back(i);
+		}
+		for (int i : pos_2) {
+			state neo;
+			if (removeAndMergeAndTestEmpty(neo, i, 2))return true;
+			pq.push(neo);
+		}
+
+		for (int i : pos_1) {
+			state neo;
+			if (removeAndMergeAndTestEmpty(neo, i, 1))return true;
+			pq.push(neo);
+		}
 		return false;
 	}
 	int getDepth()const {
 		return depth;
 	}
 private:
-	bool removeAtAndTryMerge(int id) {
-		return false;
+	bool removeAndMergeAndTestEmpty(state& out,int id,char type) {
+
 	}
+ 
 };
 
 
