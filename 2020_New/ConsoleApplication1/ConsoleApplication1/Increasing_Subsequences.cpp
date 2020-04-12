8f3b6aa1f650d25f14a088df4d29352762bb2112
hly <hly920928@gmail.com>
2020-04-12 23:44:10 +0800

    Update Increasing_Subsequences.cpp
    changing method,using std::set

--- 2020_New/ConsoleApplication1/ConsoleApplication1/Increasing_Subsequences.cpp
+++ 2020_New/ConsoleApplication1/ConsoleApplication1/Increasing_Subsequences.cpp
@@ -1,27 +1,40 @@
 //491. Increasing Subsequences
 
 #include "mainHeader.h"
+#include <set>
 using namespace std;
 vector<vector<int>> findSubsequences(vector<int>& nums) {
-	vector<vector<int>> temp;
+	set<vector<int>>table[20];
+	set<vector<int>> temp;
 	for (int i = 0; i < nums.size(); i++) {
-		int post_len = temp.size();
-		for (int j = 0; j < post_len; j++) {
-			auto& v = temp[j];
-			if (v[v.size() - 1] <= nums[i]) {
-				temp.push_back(v);
-				temp[temp.size() - 1].push_back(nums[i]);
+ 
+		for (int j = 1; j <=15; j++) {
+			auto& now_set = table[j];
+			auto& next_set = table[j+1];
+			for (auto& v : now_set) {
+		 
+				if (v[v.size() - 1] <= nums[i]) {
+					vector<int> neo = v;
+					neo.push_back(nums[i]);
+					auto itr = next_set.find(neo);
+					if(itr==next_set.end())temp.insert(neo);
+				}
 			}
+		
 	   }
-		temp.push_back(vector<int>({ nums[i] }));
-	}
-	sort(temp.begin(), temp.end());
-	vector<vector<int>> ans;
-	for (int i = 1; i < temp.size(); i++) {
-		auto& v = temp[i];
-		if (v.size() >= 2 && v != temp[i - 1]) {
-			ans.push_back(v);
+		table[1].insert(vector<int>({ nums[i] }));
+		for (auto& v : temp) {
+			int len = v.size();
+			table[len].insert(v);
 		}
+
+
+		temp.clear();
+	}
+	 
+	vector<vector<int>> ans;
+	for (int i =2; i <15; i++) {
+		for (auto& v : table[i])ans.push_back(v);
 	}
 	return ans;
 }
