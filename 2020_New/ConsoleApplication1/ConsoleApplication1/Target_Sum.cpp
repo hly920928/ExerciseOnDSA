fb0fe7b46f6551622453abbf3306e20990bacd44
hly <hly920928@gmail.com>
2020-04-23 18:33:40 +0800

    Update Target_Sum.cpp
    finished,but not fully tested

--- 2020_New/ConsoleApplication1/ConsoleApplication1/Target_Sum.cpp
+++ 2020_New/ConsoleApplication1/ConsoleApplication1/Target_Sum.cpp
@@ -5,15 +5,31 @@
 using namespace std;
 
 int* _ans;
-vector<int>* _nums;
+vector<int>* _nums_1;
 vector<int>* _part_sum;
 int Sum;
 void findTargetSumWays_re(int pos, int now_sum) {
-
+	int& ans = *_ans;
+	vector<int>& nums = *_nums_1;
+	vector<int>& part_sum= * _part_sum;
+	if (pos == nums.size() - 1) {
+		if(now_sum+ nums[pos]==Sum|| now_sum -nums[pos] == Sum)ans++;
+		return;
+	}
+	if (now_sum + part_sum[pos] == Sum) {
+		ans++; return;
+	}
+	if (now_sum- part_sum[pos] == Sum) {
+		ans++; return;
+	}
+	if (now_sum + part_sum[pos]<Sum)return;
+	if (now_sum - part_sum[pos] >Sum)return;
+	findTargetSumWays_re(pos + 1, now_sum + nums[pos]);
+	findTargetSumWays_re(pos + 1, now_sum - nums[pos]);
 }
 int findTargetSumWays(vector<int>& nums, int S) {
 	int ans = 0; _ans = &ans; Sum = S;
-	_nums = &nums;
+	_nums_1 = &nums;
 	int sum = 0;
 	vector<int> part_sum; _part_sum = &part_sum;
 	part_sum.resize(nums.size());
