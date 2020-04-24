ae5834d8d18cda367c51db7aaba8ae7b667c1a36
hly <hly920928@gmail.com>
2020-04-24 15:35:18 +0800

    Update Target_Sum.cpp
    Finished ,but a little slow

--- 2020_New/ConsoleApplication1/ConsoleApplication1/Target_Sum.cpp
+++ 2020_New/ConsoleApplication1/ConsoleApplication1/Target_Sum.cpp
@@ -7,20 +7,27 @@ using namespace std;
 int* _ans;
 vector<int>* _nums_1;
 vector<int>* _part_sum;
+vector<int>* _count_zero;
 int Sum;
 void findTargetSumWays_re(int pos, int now_sum) {
 	int& ans = *_ans;
 	vector<int>& nums = *_nums_1;
 	vector<int>& part_sum= * _part_sum;
+	vector<int>& count_zero = *_count_zero;
 	if (pos == nums.size() - 1) {
-		if(now_sum+ nums[pos]==Sum|| now_sum -nums[pos] == Sum)ans++;
+		if(now_sum+ nums[pos]==Sum)ans++;
+		if(now_sum - nums[pos] == Sum)ans++;
 		return;
 	}
+	if (part_sum[pos] == 0) {
+		if (now_sum == Sum)ans += pow(2, count_zero[pos]);
+		return ;
+	}
 	if (now_sum + part_sum[pos] == Sum) {
-		ans++; return;
+		ans+= pow(2, count_zero[pos]); return;
 	}
 	if (now_sum- part_sum[pos] == Sum) {
-		ans++; return;
+		ans+= pow(2, count_zero[pos]); return;
 	}
 	if (now_sum + part_sum[pos]<Sum)return;
 	if (now_sum - part_sum[pos] >Sum)return;
@@ -32,10 +39,14 @@ int findTargetSumWays(vector<int>& nums, int S) {
 	_nums_1 = &nums;
 	int sum = 0;
 	vector<int> part_sum; _part_sum = &part_sum;
-	part_sum.resize(nums.size());
+	vector<int> count_zero; _count_zero = &count_zero;
+	part_sum.resize(nums.size()); count_zero.resize(nums.size());
+	int countZero = 0;
 	for (int i = nums.size() - 1; i >= 0; i--) {
 		sum += nums[i];
 		part_sum[i] = sum;
+		if (nums[i] == 0)countZero++;
+		count_zero[i] = countZero;
 	}
 	findTargetSumWays_re(0, 0);
 	return ans;
