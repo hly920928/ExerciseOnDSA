4cc8f5ee5a9de2edc7a8bae23ea6a28a6bcc429c
hly <hly920928@gmail.com>
2020-05-01 18:18:14 +0800

    Update Teemo_Attacking.cpp
    Linear Search, a liittle slower

--- 2020_New/ConsoleApplication1/ConsoleApplication1/Teemo_Attacking.cpp
+++ 2020_New/ConsoleApplication1/ConsoleApplication1/Teemo_Attacking.cpp
@@ -5,6 +5,7 @@
  
 using namespace std;
 int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+    if (timeSeries.size() == 0)return 0;
     int ans = 0;
     for (int i = 1; i < timeSeries.size(); i++)ans += min(timeSeries[i] - timeSeries[i - 1], duration);
     ans += duration;
